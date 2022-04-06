4214279c207ef021ddfc04dc75b069e7c68df1d0
clintonjosephs <clintonjosephs26@gmail.com>
2022-04-06 16:06:19 +0100

    fixed linters errors

--- person.rb
+++ person.rb
@@ -4,7 +4,7 @@ class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id, :rentals
 
-  def initialize(age, name = 'Unknown', parent_permission = true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super()
     @id = Random.rand(1..1000)
     @name = name
@@ -27,4 +27,3 @@ class Person < Nameable
 
   private :of_age?
 end
-
