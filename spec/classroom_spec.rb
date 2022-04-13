a1879f186e529d2b1b4003aadbf6f9cdc50b1abe
unknown <mugishasamuel42@gmail.com>
2022-04-13 06:17:27 -0700

    Fix linter errors

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -1,17 +1,14 @@
 require_relative '../classes/classroom'
 
 describe Classroom do
+  before :each do
+    @classroom = Classroom.new('AB')
+  end
+  it 'it should return classroom instance' do
+    expect(@classroom).to be_instance_of Classroom
+  end
 
-
-    before :each do
-        @classroom= Classroom.new('AB')
-
-    end
-    it 'it should return classroom instance' do
-        expect(@classroom).to be_instance_of Classroom
-    end
-
-    it 'it should return AB' do
-        expect(@classroom.label).to eql('AB')
-    end
+  it 'it should return AB' do
+    expect(@classroom.label).to eql('AB')
+  end
 end
--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -1,23 +1,21 @@
-require_relative '../classes/teacher.rb'
+require_relative '../classes/teacher'
 
 describe Teacher do
+  before :each do
+    @teacher = Teacher.new('math', 19, 'sam')
+  end
 
-    before :each do
-        @teacher= Teacher.new('math',19,'sam')
-    end
+  it 'it should return true' do
+    expect(@teacher.can_use_services?).to be true
+  end
 
-    it 'it should return true' do
-      expect(@teacher.can_use_services?).to be true 
-    end
+  it 'it should return parameters' do
+    expect(@teacher.specialization).to eql('math')
+    expect(@teacher.age).to be(19)
+    expect(@teacher.name).to eql('sam')
+  end
 
-    it 'it should return parameters' do
-       expect(@teacher.specialization).to eql('math')
-       expect(@teacher.age).to be(19)
-       expect(@teacher.name).to eql('sam') 
-    end
-
-    it 'it should return instance' do
-        expect(@teacher).to be_instance_of Teacher
-    end
+  it 'it should return instance' do
+    expect(@teacher).to be_instance_of Teacher
+  end
 end
-
