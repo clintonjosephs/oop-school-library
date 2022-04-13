d409de31acef8484fe6b46d03bd54c6efb7c19f7
unknown <mugishasamuel42@gmail.com>
2022-04-13 05:51:01 -0700

    Add teacher test

--- classes/teacher.rb
+++ classes/teacher.rb
@@ -4,7 +4,7 @@ class Teacher < Person
   attr_reader :specialization
 
   def initialize(specialization, age, name = 'Unknown', parent_permission: true)
-    super(name, age, parent_permission: parent_permission)
+    super(age, name, parent_permission: parent_permission)
     @specialization = specialization
   end
 
